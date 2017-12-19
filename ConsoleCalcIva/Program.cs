8b8917ab777250fbcfa26d8aca64089435e9e3bf
TrapScriptMotion <zezeballoon@gmail.com>
2017-12-19 20:42:41 +0000

    ca

--- ConsoleCalcIva/Program.cs
+++ ConsoleCalcIva/Program.cs
@@ -9,6 +9,23 @@ namespace ConsoleCalcIva
     {
         static void Main(string[] args)
         {
+            double totalSemIva = 0;
+            double valorIva = 0.23;
+            double totalComIva = 0;
+            const double percentagemIva = 0.23;
+
+            Console.Write("Digite o valor a calcular: ");
+            totalSemIva = double.Parse(Console.ReadLine());
+
+            valorIva = totalSemIva * percentagemIva;
+            totalComIva = totalSemIva + valorIva;
+
+            Console.Write("O valor com Iva é: " + totalComIva);
+            Console.Read();
+
+            /*Final da listagem
+            Código criado por Buno Gonçalves 
+            @Dezembro 2017*/
         }
     }
 }
