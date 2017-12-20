df2f1b3e3a0c297a4e99caf450b070d8ced65d56
TrapMotion <zezeballoon@gmail.com>
2017-12-20 13:03:15 +0000

    Cálculo do IVA 23%

--- ConsoleCalcIva/Program.cs
+++ ConsoleCalcIva/Program.cs
@@ -14,13 +14,11 @@ namespace ConsoleCalcIva
             double valorIva = 0.23;
             double totalComIva = 0;
             const double percentagemIva = 0.23;
-
             Console.Write("Digite o valor a calcular: ");
             totalSemIva = double.Parse(Console.ReadLine());
 
             valorIva = totalSemIva * percentagemIva;
             totalComIva = totalSemIva + valorIva;
-
             Console.Write("O valor com Iva é: " + totalComIva);
             Console.Read();
 
